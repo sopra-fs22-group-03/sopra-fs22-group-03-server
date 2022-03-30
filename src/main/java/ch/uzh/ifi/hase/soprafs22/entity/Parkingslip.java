a5a2ebe7f9925d53c4ed4b351e464e44cd30040b
Richard Specker <91221074+rspecker@users.noreply.github.com>
2022-03-30 16:04:46 +0200

    created parkingslip class

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/entity/Parkingslip.java
@@ -0,0 +1,101 @@
+package ch.uzh.ifi.hase.soprafs22.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.*;
+
+/**
+ * Internal Parkingslip Representation
+ * This class composes the internal representation of the parkingslip and defines how
+ * a parkingslip is stored in the database.
+ * Every variable will be mapped into a database field with the @Column
+ * annotation
+ * - nullable = false -> this cannot be left empty
+ * - unique = true -> this value must be unqiue across the database -> composes
+ * the primary key
+ */
+@Entity
+@Table(name = "PARKINGSLIP")
+
+public class Parkingslip implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private long parkingslipId;
+
+    @Column(nullable = false)
+    private long carparkId;
+
+    @Column(nullable = false)
+    private long userId;
+
+    @Column(nullable = false)
+    private String checkinDate;
+
+    @Column(nullable = false)
+    private String checkinTime;
+
+    @Column(nullable = true)
+    private String checkoutDate;
+
+    @Column(nullable = true)
+    private String checkoutTime;
+
+    public long getParkingslipId() {
+        return parkingslipId;
+    }
+
+    public void setParkingslipId(long parkingslipId) {
+        this.parkingslipId = parkingslipId;
+    }
+
+    public long getCarparkId() {
+        return carparkId;
+    }
+
+    public void setCarparkId(long carparkId) {
+        this.carparkId = carparkId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getCheckinDate() {
+        return checkinDate;
+    }
+
+    public void setCheckinDate(String checkinDate) {
+        this.checkinDate = checkinDate;
+    }
+
+    public String getCheckinTime() {
+        return checkinTime;
+    }
+
+    public void setCheckinTime(String checkinTime) {
+        this.checkinTime = checkinTime;
+    }
+
+    public String getCheckoutDate() {
+        return checkoutDate;
+    }
+
+    public void setCheckoutDate(String checkoutDate) {
+        this.checkoutDate = checkoutDate;
+    }
+
+    public String getCheckoutTime() {
+        return checkoutTime;
+    }
+
+    public void setCheckoutTime(String checkoutTime) {
+        this.checkoutTime = checkoutTime;
+    }
+
+}
