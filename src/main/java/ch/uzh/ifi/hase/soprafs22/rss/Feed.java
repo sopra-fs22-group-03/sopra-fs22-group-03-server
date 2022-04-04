92102ff5470c0a825c857ed66e5365624e3f524b
Richard Specker <91221074+rspecker@users.noreply.github.com>
2022-04-04 19:28:59 +0200

    RSS-Feed Parser
    To do: RSS parser has to initiated.

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/rss/Feed.java
@@ -0,0 +1,47 @@
+package ch.uzh.ifi.hase.soprafs22.rss;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Stores an RSS feed
+ */
+public class Feed {
+
+    final String title;
+    final String link;
+    final String description;
+    // final String date;
+
+    final List<FeedMessage> entries = new ArrayList<FeedMessage>();
+
+    public Feed(String title, String link, String description) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        //  this.date = date;
+    }
+
+    public List<FeedMessage> getMessages() {
+        return entries;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Feed [description=" + description
+                + ", link=" + link + ", title=" + title + "]";
+    }
+
+}
