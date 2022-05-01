413d5008d27201426a193f2a0f868742de230f57
e-schuh <eliassebastian.schuhmacher@uzh.ch>
2022-05-01 17:52:01 +0200

    added userService tests

--- src/test/java/ch/uzh/ifi/hase/soprafs22/service/UserServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/service/UserServiceIntegrationTest.java
@@ -1,80 +1,109 @@
-///**
-// * TESTS outcommented
-// * Need to be adapted to current entities
-// */
-//
-//package ch.uzh.ifi.hase.soprafs22.service;
-//
-//import ch.uzh.ifi.hase.soprafs22.entity.User;
-//import ch.uzh.ifi.hase.soprafs22.repository.UserRepository;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//import org.springframework.web.server.ResponseStatusException;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-///**
-// * Test class for the UserResource REST resource.
-// *
-// * @see UserService
-// */
-//@WebAppConfiguration
-//@SpringBootTest
-//public class UserServiceIntegrationTest {
-//
-//  @Qualifier("userRepository")
-//  @Autowired
-//  private UserRepository userRepository;
-//
-//  @Autowired
-//  private UserService userService;
-//
-//  @BeforeEach
-//  public void setup() {
-//    userRepository.deleteAll();
-//  }
-//
-//  @Test
-//  public void createUser_validInputs_success() {
-//    // given
-//    assertNull(userRepository.findByUsername("testUsername"));
-//
-//    User testUser = new User();
-//    testUser.setPassword("password");
-//    testUser.setUsername("testUsername");
-//
-//    // when
-//    User createdUser = userService.createUser(testUser);
-//
-//    // then
-//    assertEquals(testUser.getUserId(), createdUser.getUserId());
-//    assertEquals(testUser.getPassword(), createdUser.getPassword());
-//    assertEquals(testUser.getUsername(), createdUser.getUsername());
-//    assertNotNull(createdUser.getToken());
-////    assertEquals(true, createdUser.getLogged_in()); //User is online after registration
-//  }
-//
-//  @Test
-//  public void createUser_duplicateUsername_throwsException() {
-//    assertNull(userRepository.findByUsername("testUsername"));
-//
-//    User testUser = new User();
-//    testUser.setPassword("password");
-//    testUser.setUsername("testUsername");
-//    User createdUser = userService.createUser(testUser);
-//
-//    // attempt to create second user with same username
-//    User testUser2 = new User();
-//
-//    // change the name but forget about the username
-//    testUser2.setPassword("password2");
-//    testUser2.setUsername("testUsername");
-//
-//    // check that an error is thrown
-//    assertThrows(ResponseStatusException.class, () -> userService.createUser(testUser2));
-//  }
-//}
+package ch.uzh.ifi.hase.soprafs22.service;
+
+import ch.uzh.ifi.hase.soprafs22.entity.User;
+import ch.uzh.ifi.hase.soprafs22.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for the UserResource REST resource.
+ *
+ * @see UserService
+ */
+@WebAppConfiguration
+@SpringBootTest
+public class UserServiceIntegrationTest {
+
+  @Qualifier("userRepository")
+  @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
+  private UserService userService;
+
+  @BeforeEach
+  public void setup() {
+    userRepository.deleteAll();
+  }
+
+  private User user;
+
+  @Test
+  public void createUser_validInputs_success() {
+
+      // given
+      assertNull(userRepository.findByUsername("testUsername"));
+
+      user = new User();
+      user.setPassword("password");
+      user.setUsername("testUsername");
+      user.setStreet("Musterstrasse");
+      user.setStreetNo("1");
+      user.setZipCode(8000L);
+      user.setCity("Zurich");
+      user.setEmail("tes@test.ch");
+      user.setPhoneNumber("'0790000001'");
+      user.setLicensePlate("ZH1");
+      user.setCreditCardNumber(1111111111111111L);
+
+      // when
+      User createdUser = userService.createUser(user);
+
+      // then
+      assertEquals(user.getId(), createdUser.getId());
+      assertEquals(user.getPassword(), createdUser.getPassword());
+      assertEquals(user.getUsername(), createdUser.getUsername());
+      assertEquals(user.getStreet(), createdUser.getStreet());
+      assertEquals(user.getStreetNo(), createdUser.getStreetNo());
+      assertEquals(user.getZipCode(), createdUser.getZipCode());
+      assertEquals(user.getCity(), createdUser.getCity());
+      assertEquals(user.getEmail(), createdUser.getEmail());
+      assertEquals(user.getPhoneNumber(), createdUser.getPhoneNumber());
+      assertEquals(user.getLicensePlate(), createdUser.getLicensePlate());
+      assertEquals(user.getCreditCardNumber(), createdUser.getCreditCardNumber());
+      assertFalse(createdUser.getIsManager());
+      assertTrue(createdUser.getIsLoggedIn());
+      assertNotNull(createdUser.getToken());
+  }
+
+  @Test
+  public void createUser_duplicateUsername_throwsException() {
+      assertNull(userRepository.findByUsername("testUsername"));
+
+      user = new User();
+      user.setPassword("password");
+      user.setUsername("testUsername");
+      user.setStreet("Musterstrasse");
+      user.setStreetNo("1");
+      user.setZipCode(8000L);
+      user.setCity("Zurich");
+      user.setEmail("tes@test.ch");
+      user.setPhoneNumber("'0790000001'");
+      user.setLicensePlate("ZH1");
+      user.setCreditCardNumber(1111111111111111L);
+
+      User createdUser = userService.createUser(user);
+
+      // attempt to create second user with same username
+      User user2 = new User();
+      user2.setPassword("password2");
+      user2.setUsername("testUsername");
+
+      //try to create a new user with already existing username; status error with code 400 should be thrown
+      try {
+          userService.createUser(user2);
+          Assertions.fail("BAD REQUEST exception should have been thrown!");
+      }
+      catch (ResponseStatusException ex) {
+          assertEquals(400, ex.getRawStatusCode());
+      }
+  }
+}
--- src/test/java/ch/uzh/ifi/hase/soprafs22/service/UserServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/service/UserServiceTest.java
@@ -1,79 +1,98 @@
-///**
-// * TESTS outcommented
-// * Need to be adapted to current entities
-// */
-//
-//package ch.uzh.ifi.hase.soprafs22.service;
-//
-//import ch.uzh.ifi.hase.soprafs22.entity.User;
-//import ch.uzh.ifi.hase.soprafs22.repository.UserRepository;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.web.server.ResponseStatusException;
-//
-//import java.time.*;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class UserServiceTest {
-//
-//  @Mock
-//  private UserRepository userRepository;
-//
-//  @InjectMocks
-//  private UserService userService;
-//
-//  private User testUser;
-//
-//  @BeforeEach
-//  public void setup() {
-//    MockitoAnnotations.openMocks(this);
-//
-//    // given
-//    testUser = new User();
-//    testUser.setUserId(1L);
-//    testUser.setPassword("password");
-//    testUser.setUsername("testUsername");
-////    testUser.setCreationDate(LocalDate.now());
-//
-//    // when -> any object is being save in the userRepository -> return the dummy
-//    // testUser
-//    Mockito.when(userRepository.save(Mockito.any())).thenReturn(testUser);
-//  }
-//
-//  @Test
-//  public void createUser_validInputs_success() {
-//    // when -> any object is being save in the userRepository -> return the dummy
-//    // testUser
-//    User createdUser = userService.createUser(testUser);
-//
-//    // then
-//    Mockito.verify(userRepository, Mockito.times(1)).save(Mockito.any());
-//
-//    assertEquals(testUser.getUserId(), createdUser.getUserId());
-//    assertEquals(testUser.getPassword(), createdUser.getPassword());
-//    assertEquals(testUser.getUsername(), createdUser.getUsername());
-//    assertNotNull(createdUser.getToken());
-////    assertEquals(true, createdUser.getLogged_in()); //User is ONLINE after registration
-//  }
-//
-//
-//  @Test
-//  public void createUser_duplicateInputs_throwsException() {
-//    // given -> a first user has already been created
-//    userService.createUser(testUser);
-//
-//    // when -> setup additional mocks for UserRepository
-//    //Mockito.when(userRepository.findByName(Mockito.any())).thenReturn(testUser);
-//    Mockito.when(userRepository.findByUsername(Mockito.any())).thenReturn(testUser);
-//
-//    // then -> attempt to create second user with same user -> check that an error
-//    // is thrown
-//    assertThrows(ResponseStatusException.class, () -> userService.createUser(testUser));
-//  }
-//
-//}
+package ch.uzh.ifi.hase.soprafs22.service;
+
+import ch.uzh.ifi.hase.soprafs22.entity.User;
+import ch.uzh.ifi.hase.soprafs22.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+  @Mock
+  private UserRepository userRepository;
+
+  @InjectMocks
+  private UserService userService;
+
+  private User user;
+
+  @BeforeEach
+  public void setup() {
+      // given
+      user = new User();
+      user.setPassword("password");
+      user.setUsername("testUsername");
+      user.setStreet("Musterstrasse");
+      user.setStreetNo("1");
+      user.setZipCode(8000L);
+      user.setCity("Zurich");
+      user.setEmail("tes@test.ch");
+      user.setPhoneNumber("'0790000001'");
+      user.setLicensePlate("ZH1");
+      user.setCreditCardNumber(1111111111111111L);
+      user.setIsLoggedIn(true);
+      user.setIsManager(false);
+      user.setToken("'14527952-3ce3-465e-9674-b7ef35d02911'");
+      user.setId(1L);
+
+      // when -> any object is being save in the userRepository -> return the dummy
+      Mockito.when(userRepository.save(Mockito.any())).thenReturn(user);
+  }
+
+  @Test
+  public void createUser_validInputs_success() {
+      // when
+      User createdUser = userService.createUser(user);
+
+      // then
+      Mockito.verify(userRepository, Mockito.times(1)).save(Mockito.any());
+
+      assertEquals(user.getId(), createdUser.getId());
+      assertEquals(user.getPassword(), createdUser.getPassword());
+      assertEquals(user.getUsername(), createdUser.getUsername());
+      assertEquals(user.getStreet(), createdUser.getStreet());
+      assertEquals(user.getStreetNo(), createdUser.getStreetNo());
+      assertEquals(user.getZipCode(), createdUser.getZipCode());
+      assertEquals(user.getCity(), createdUser.getCity());
+      assertEquals(user.getEmail(), createdUser.getEmail());
+      assertEquals(user.getPhoneNumber(), createdUser.getPhoneNumber());
+      assertEquals(user.getLicensePlate(), createdUser.getLicensePlate());
+      assertEquals(user.getCreditCardNumber(), createdUser.getCreditCardNumber());
+      assertFalse(createdUser.getIsManager());
+      assertTrue(createdUser.getIsLoggedIn());
+      assertNotNull(createdUser.getToken());
+  }
+
+
+  @Test
+  public void createUser_duplicateInputs_throwsException() {
+      // given -> a first user has already been created
+      User createdUser = userService.createUser(user);
+
+      // when -> setup additional mocks for UserRepository
+      Mockito.when(userRepository.findByUsername(Mockito.any())).thenReturn(user);
+
+      //try to create a new user with already existing username; status error with code 400 should be thrown
+      try {
+          userService.createUser(user);
+          Assertions.fail("BAD REQUEST exception should have been thrown!");
+      }
+      catch (ResponseStatusException ex) {
+          assertEquals(400, ex.getRawStatusCode());
+      }
+  }
+
+
+}
